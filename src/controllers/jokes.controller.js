de3d47c21c002a9fbb4b6bc606d6cab32ddcbe79
anmol420 <anmolanand2k15@gmail.com>
2024-06-23 08:13:27 +0530

    Added Project Restructure

--- src/controllers/jokes.controller.js
+++ src/controllers/jokes.controller.js
@@ -1,7 +1,7 @@
 import asyncHandler from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import jokes from "../public/jokes.json" assert { type: 'json' };
+import jokes from "../../public/jokes.json" assert { type: 'json' };
 
 const randomJoke = asyncHandler( async (req, res) => {
     return res
