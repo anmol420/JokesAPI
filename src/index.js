69474484889d08b3a3bf4fd75394269eef95752f
$GIT_USER_NAME <$GIT_USER_EMAIL>
2024-06-27 20:43:08 +0530

    Added Database Connection

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 import app from "./app.js"
 
 dotenv.config({
   path: './env'
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Successfully started server on port ${process.env.PORT}.`);
-});
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Successfully started server on port ${process.env.PORT}.`);
+    });
+  })
+  .catch((e) => {
+    console.log("Database Connection Error: ", e);
+  });
+  
